484920841ad2ca6793c86c0dff5386d6b1b8c6a7
chrisdfghjkl <cdepastene@gmail.com>
2021-12-10 01:14:29 -0800

    add function passing third test

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,21 @@ function Pizza(size, premiumSauce, premiumCheese, meatTopping, veggieTopping, pr
   this.veggieTopping = veggieTopping;
   this.price = 10;
 }
+
+Pizza.prototype.calcPrice = function() {
+  if (this.size !== "Small") {
+      this.price += 2;
+  }
+  if (this.premiumSauce !== undefined) {
+      this.price += 1;
+  }
+  if (this.premiumCheese !== undefined) {
+      this.price += 1;
+  }
+  if (this.meatTopping.length > 0) {
+      this.price = this.price + this.meatTopping.length * 3;
+  }
+  if (this.veggieTopping.length > 0) {
+      this.price = this.price + this.veggieTopping.length * 2;
+  }
+}
