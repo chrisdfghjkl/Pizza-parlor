251217048da354312a35aeb183823e7a24d57370
chrisdfghjkl <cdepastene@gmail.com>
2021-12-11 00:28:31 -0800

    Add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -23,8 +23,36 @@ Pizza.prototype.calcPrice = function() {
       this.price = this.price + this.meatTopping.length * 3;
   }
   if (this.veggieTopping.length > 0) {
-      this.price = this.price + this.veggieTopping.length * 2;
+      this.price = this.price + this.veggieTopping.length * 2; // ----------- meat and veggie choices need to push to array for method to work 
   }
 }
 
 // User Interface Logic ----------------
+
+$(document).ready(function() {
+  $("form#pie-builder").submit(function(event) {
+    event.preventDefault();
+
+    const pizzaSize = $("select#pie-size").val();
+    const pizzaSauce = $("select#pie-sauce").val();
+    const pizzaCheese = $("select#pie-cheese").val();
+    const pizzaMeat = [];
+    const pizzaVeg = [];
+    const pizzaPrice = 10;
+
+    /* Template: $("input:checkbox[name=type]:checked").each(function(){
+    yourArray.push($(this).val()); ---------- to test, bookmark stackoverflow page if works */
+
+    $("input:checkbox[name=meat]:checked").each(function(){
+      pizzaMeat.push($(this).val());
+    });
+    
+    $("input:checkbox[name=veg]:checked").each(function(){
+      pizzaVeg.push($(this).val());
+    });
+
+    let pizzaOrder = new Pizza(pizzaSize, pizzaSauce, pizzaCheese, pizzaMeat, pizzaVeg, pizzaPrice);
+    let finalTotal = pizzaOrder.calcPrice();
+
+  });
+});
